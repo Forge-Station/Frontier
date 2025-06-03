20b9107eee9b6f59e548cf2e656e9ace9ba2dadb
Mr_Lop_Lop <sem3667@gmail.com>
2025-06-04 00:19:09 +0700

    ?

--- Content.Shared/_NF/CCVar/NFCCVars.cs
+++ Content.Shared/_NF/CCVar/NFCCVars.cs
@@ -1,9 +1,10 @@
+using Robust.Shared;
 using Robust.Shared.Configuration;
 
 namespace Content.Shared._NF.CCVar;
 
 [CVarDefs]
-public sealed class NFCCVars
+public sealed class NFCCVars : CVars
 {
     /*
      *  Respawn
